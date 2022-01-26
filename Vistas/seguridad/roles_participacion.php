3bda8492cc2d593b0064689fcb987b1506f2dc5a
Any Hernandez <hmeli90@yahoo.es>
2022-01-26 22:17:29 +0100

    lo siento cree una clone nueva pero se me olvido borrar la rama anterior

--- Vistas/seguridad/roles_participacion.php
+++ Vistas/seguridad/roles_participacion.php
@@ -16,7 +16,7 @@
                      </div>
                      <div class="col-sm-4">
                         <div class="form-group">
-                            <label>Area a pertenecer</label>
+                            <label>Área a pertenecer</label>
                             <input type="text" name="nombre2" class="form-control"  required>
                         </div>
                      </div>
@@ -24,10 +24,45 @@
 
                   <section class="submission">
                 <!-- El button envia todos los inputs de este  formulario al archivo especificado dentro del atributo action-->
-                       <input type="submit" name="registrarse">
+                  <input class="btn btn-primary btn-lg" type="submit" name="registrarse" style="float: right;">
                   </section>
-
-
+                  <!--la tabla comienza aqui -->
+                  <setion class="content">
+                  <div class="container-fluid">
+                  <div claas="row">
+                  <div class ="col-12">
+                  <div class="card">
+                  <div class ="card-header">
+                   <h3 class="card-title">Tipos de usuarios Existente</h3>
+                  </div>
+                   <div class= "card-body">
+                   <table id="example2" class="table table-bordered table-hover">
+                     <thead>
+                      <tr>
+                      <th>Tipo de Usuario </th>
+                      <th> Área que pertenece </th>
+                      </tr>
+                     </thead>
+                     <tbody>
+                     <tr>
+                     <td>Administrador</td>
+                     <td>Administración</td>
+                     </tr>
+                     <tr>
+                     <td>Secretaria</td>
+                     <td>Administración</td>
+                     </tr>                  
+                     </tbody>
+                     
+                   </table>
+                   </div>
+                  </div>
+                  </div>
+                  </div>
+                 </div> 
+                  <!--la tabla termina aqui -->            
+                 </setion>
+ 
                 </form>
               </div>
               <!-- /.card-body -->
