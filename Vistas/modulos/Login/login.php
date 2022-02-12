daff074602e3db0b1c956ed987aa33029405df9f
luzmontoya07 <97485283+luzmontoya07@users.noreply.github.com>
2022-02-12 01:42:57 -0600

    funciona el boton salir, se ve la imagen del login

--- Vistas/modulos/Login/login.php
+++ Vistas/modulos/Login/login.php
@@ -4,13 +4,11 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="../fontawesome-free/css/all.min.css">
     <title>Login Usuario</title>
   </head>
-
   <style>
        body{
            background-color:#E8E8E1;
@@ -18,9 +16,8 @@
        }
        .bg{
         background-size: cover;
-         background-image:url(../assets/Vistas/modulos/imagenes/fondo_login.jpg);
+         background-image:url(../../../assets/imagenes/fondo_login.jpg);
          background-position: center center;
-
        }
   </style>
   <body>
@@ -42,10 +39,6 @@
                       <span    class="input-group-text" id=""><i class="fas fa-lock"></i></span> 
                       <input type="password" name="contrasena" class="form-control" placeholder="Ingresa tu contrase&ntilde;a" aria-label="Username" aria-describedby="basic-addon1">
                   </div>
-                  <div class="mb-4">
-                     <input type="checkbox"class="form-check-input">
-                     <label class="form-check-label">Recuerdame</label>
-                  </div>
                   <div class="d-grid">
                       <button type="submit" class="btn btn-danger btn-block">INGRESAR</button>
                   </div>
@@ -58,10 +51,7 @@
             </div>
         </div>
     <div>
-
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
