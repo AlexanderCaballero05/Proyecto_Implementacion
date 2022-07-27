84760022bd308aa462bc3156bff47982863dfb21
AlexanderCaballero05 <aacaballero@unah.hn>
2022-07-27 00:28:11 -0600

    modificacion en agregar usuarios

--- Vistas/modulos/recuperacion_clave_preguntas.php
+++ Vistas/modulos/recuperacion_clave_preguntas.php
@@ -144,10 +144,10 @@ ANY HERNANDEZ       01-JUN-2022       PARAMETROS DE MAXIMO Y MINIMO DE LA RESPUE
         })()
   </script>
   <script>
-      function soloLetras(e){
+      function soloLetrasComa(e){
        key = e.keyCode || e.which;
        tecla = String.fromCharCode(key).toLowerCase();
-       letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+       letras = " áéíóúabcdefghijklmnñopqrstuvwxyz ,.";
        especiales = ["8-37-39-46"];
        tecla_especial = false
        for(var i in especiales){
