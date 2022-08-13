828985c1bc26b45c13f5de90295785de7b16fb3f
Diana Rut <93566502+DIANARUTGARCIA@users.noreply.github.com>
2022-08-12 23:50:51 -0600

    Nuevos cambios

--- Vistas/modulos/ProcesoCitasPsicologicas.php
+++ Vistas/modulos/ProcesoCitasPsicologicas.php
@@ -176,9 +176,8 @@ $codigoObjeto = 32;///CAMBIAR
                                                         <div class="modal-dialog modal-lg">
                                                             <div class="modal-content">
                                                                 <div class="modal-header" style="background-color: #0CCDE3">
-                                                                
                                                                     <h4 class="text-center">Ver cita  Paso <i class="bi bi-1-circle"></i> <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-1-circle" viewBox="0 0 16 16">
-                                                                <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8Zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM9.283 4.002V12H7.971V5.338h-.065L6.072 6.656V5.385l1.899-1.383h1.312Z"/>
+                                                                      <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8Zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM9.283 4.002V12H7.971V5.338h-.065L6.072 6.656V5.385l1.899-1.383h1.312Z"/>
                                                                 </svg></h4>
                                                                     
                                                                     <button type="button" class="close" data-dismiss="modal">&times;</button>
