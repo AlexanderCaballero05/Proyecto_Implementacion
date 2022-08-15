e514554010c14d797984a78d07513934eb1300f7
AlexanderCaballero05 <aacaballero@unah.hn>
2022-08-15 13:37:51 -0600

    Proceso citas espirituales

--- Vistas/modulos/procesoCitasEspirituales.php
+++ Vistas/modulos/procesoCitasEspirituales.php
@@ -95,6 +95,12 @@ include_once 'conexionpdo.php';
                         <div class="card-header text-center" style="background-color: #F7F8F9";>
                             <!-- TITULO ENCABEZADO DATOS PERSONALES -->
                         </div>
+                        <div class="alert alert" style="border-color:blue">
+              <h3 class="text-center"> Paso <i class="bi bi-2-circle"></i><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-2-circle" viewBox="0 0 16 16">
+                <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8Zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM9.283 4.002V12H7.971V5.338h-.065L6.072 6.656V5.385l1.899-1.383h1.312Z"/>
+              </svg>
+              </h3>
+            </div>
                         <!-- form start -->
                         <div class="card-body">
                             <div class="table-responsive">
